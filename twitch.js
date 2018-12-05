92de8e09d57bc1d3b89397b7d756726bc653f5b4
awbait <awbait.com@gmail.com>
2018-12-06 01:35:34 +0300

    Update twitch.js
    Изменение команд !reg и !points

--- twitch.js
+++ twitch.js
@@ -24,7 +24,7 @@ client.on('connected', (address, port) => {
 })
 
 client.on('chat', (channel, user, message, self) => {
-	if (self) return; // Не читать мои сообщения
+	if (self) return;
 
 	switch (message) {
 		case '!reg':
@@ -32,7 +32,8 @@ client.on('chat', (channel, user, message, self) => {
 				if(data) {
 					say(`${user['display-name']} вы уже зарегестрированы в системе.`);
 				} else { 
-					db.createNewUserByTID(user).then(() => {
+					console.log('Создать пользователя');
+					db.createUserByTID(user).then(() => {
 						say(`${user['display-name']} вы успешно зарегестрированы в системе.`);
 					})
 				}
@@ -40,44 +41,16 @@ client.on('chat', (channel, user, message, self) => {
 			break;
 		case '!points':
 			let points;
-			db.getUserByTID(user['user-id']).then((data) => { // сделать проверку зарегистрирован ли пользователь
-				points = data.coins;
-				say(`${user['display-name']} у вас на счету ${points} монет.`)
+			db.getUserByTID(user['user-id']).then((data) => {
+				points = data.amount;
+				say(`${user['display-name']} у вас на счету ${points} поинтов.`)
 			});
 			break;
 		default:
 			break;
 	}
-
 })
-
 
 function say(text) {
 	client.say('awbait', text);
 }
-/*
-client.on('chat', (channel, user, message, self) => { // TODO: Добавить проверку на фоллоу канала
-    if (message === '!reg') {
-		sendModuleMsg(process, 'db', 'reg-check', user);
-    } else if (message === '!coins') {
-		sendModuleMsg(process, 'bd', 'get-coins', user)
-	}
-})
-
-process.on('message', (msg) => {
-    switch (msg.header) {
-        case 'user-exists':
-			client.action('awbait', msg.data['display-name'] +' Вы уже зарегистрированы!');
-			break;
-		case 'user-registered':
-			client.action('awbait', msg.data['display-name'] +' Вы успешно зарегистрированы!');
-			break;
-		case 'user-coins':
-			client.action('awbait', msg.data[0]['display-name'] + ' На вашем аккаунте: ' + msg.data[1] + ' бабла.')
-			break;
-        default:
-            break;
-    }
-});
-
-*/
