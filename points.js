3822b2ff2d7f3a12cd64171d44721510da4577f9
awbait <awbait.com@gmail.com>
2018-12-08 01:55:04 +0300

    points system upd

--- points.js
+++ points.js
@@ -1,12 +1,12 @@
-const request = require("request");
+const request = require('request-promise');
 const db = require('./db');
 
 let chatters;
 
 function getChatters() {
-  var options = {
+  let options = {
     method: 'GET',
-    url: 'https://tmi.twitch.tv/group/user/dicik_/chatters',
+    url: 'https://tmi.twitch.tv/group/user/exxxe/chatters',
     json: true
   };
 
@@ -16,24 +16,58 @@ function getChatters() {
     chatters = body.chatters.viewers;
     check();
   });
-
 }
-setInterval (() => {
-  getChatters()
-}, 60000);
 
+async function _performGetRequest(url, qs) {
+  try {
+    var response = await request({
+      url: url,
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer m34gekadods38wxg14f7g64lt0dlz7'
+      },
+      qs: qs,
+      json: true
+    });
+
+    if (!response || !response.data) {
+      return [];
+    } else {
+      return response.data;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getUsers(options) {
+  try {
+    return await _performGetRequest.call(this,
+      'https://api.twitch.tv/helix/users',
+      options
+    );
+  } catch (error) {
+    throw err;
+  }
+}
+
+setInterval(() => {
+  getChatters();
+}, 60000);
 
 async function check() {
   for (key in chatters) {
-    await db.getUserPoints(chatters[key]).then( async (data) => {
-      if(data) {
-        await db.addUserPoints(chatters[key]).then(async () => {
+    await db.getUserByTU(chatters[key]).then(async (data) => {
+      if (data) {
+        await db.addUserPoints(data.id).then(async () => {
           console.log(`Данные обновлены ${chatters[key]}`);
         })
       } else {
-        await db.createUserPoints(chatters[key], 1).then( async () => {
-          console.log(`Пользователь ${chatters[key]} добавлен.`);
-        })
+        let user = await getUsers({
+          login: chatters[key]
+        });
+        await db.createUserByTID(user[0].login, user[0].id);
+        console.log(`Пользователь ${chatters[key]} добавлен.`);
       }
     });
   }
